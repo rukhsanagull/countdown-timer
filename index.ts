c36fb7811b1a4821fdd8df81dc73f1731ced8fdd
rukhsanagull <164994220+rukhsanagull@users.noreply.github.com>
2024-05-07 22:08:59 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+import {differenceInSeconds, interval} from "date-fns"
+
+const res = await inquirer.prompt(
+    {
+        name:"userInput",
+        type:"number",
+        message:"Enter the amount of seconds"
+    }
+);
+
+let input = res.userInput
+
+function startTime (val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval((()=>{
+        const currTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime, currTime);
+        if(timeDiff <= 0){
+            console.log("Time is expired");
+           process.exit()
+        }
+        const min = Math.floor((timeDiff%(3600*24))/3600)
+        const sec = Math.floor(timeDiff%60)
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`);
+        
+    }),1000)
+}
+
+startTime(input)
